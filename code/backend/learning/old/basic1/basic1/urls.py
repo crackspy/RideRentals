9476e3bdc34625f3046a531a7ef58ea9190fd3d5
Crack Spy <crackspy.log232@gmail.com>
2024-12-12 09:40:13 +0530

    .

--- code/backend/learning/old/basic1/basic1/urls.py
+++ code/backend/learning/old/basic1/basic1/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
-    path('test-hello/', views.print_hello),
-    path('hello/', views.hello_page),
-    path('one/', views.one_page),
-    path('image/', views.show_image),
+    path('app/', include('app.urls'))
 ]
 
