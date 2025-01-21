624cf8604d234366360ed48889e1f44a0e6bd359
Crack Spy <crackspy.log232@gmail.com>
2025-01-21 06:38:42 +0530

    .

--- code/backend/main/Ride_Rentals/mainapp/urls.py
+++ code/backend/main/Ride_Rentals/mainapp/urls.py
@@ -1,22 +1,38 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='home'),
+
+    # Authentication URLs
     path('auth/', views.auth_page, name='auth'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
 
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+            template_name='mainapp/login/password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+            template_name='mainapp/login/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/uidb64/token/',auth_views.PasswordResetConfirmView.as_view(
+            template_name='mainapp/login/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+            template_name='mainapp/login/password_reset_complete.html'), name='password_reset_complete'),
+
+    # User profile URLs
     path('profile/', views.profile_dashboard, name='profile'),
 
+    # Car rental URLs
     path('cars/', views.explore_cars, name='explore_cars'),
     path('success/', views.success_page, name="success_page"),
     path('booking/<slug:slug>', views.booking, name='booking'),
     path('add-to-wishlist/<slug:slug>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove-from-wishlist/<slug:slug>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 
-
+    # test URLs
     path('test/', views.test, name='test'),
     path('test-p/', views.test_p, name='test-p'),
     path('send-test-email/', views.send_test_email, name='send_test_email'),
