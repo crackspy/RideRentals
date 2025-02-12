600488fcad6b708320fc8dd87b0a37bd7586e5af
crackspy <crackspy.log232@gmail.com>
2025-02-12 20:59:22 +0530

    v1 done -1

--- code/main/Ride_Rentals/Ride_Rentals/settings.py
+++ code/main/Ride_Rentals/Ride_Rentals/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t6ig$#+phx@%252q-m=#hh%+l)v@@9zon1#*mqh@_bel@+fi@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # for local ip
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.73.16']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', 'your-server-ip']
 
 
